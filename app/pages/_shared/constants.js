d59435f5db98ed4602287405644d0f7d464b20c1
Arthur Denner <arthurdenner7@gmail.com>
2021-06-25 15:15:59 +0200

    refactor(client): organize login code

--- app/pages/_shared/constants.js
+++ app/pages/_shared/constants.js
@@ -18,3 +18,13 @@ export const SOCKET_NAMESPACES = {
 export const PEER_CONFIG = Object.values({
   id: undefined,
 });
+
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyD0vyC-dOjj-F-ewtvKdqkoS7jCpMwnWwM',
+  authDomain: 'semanajsexpert4.firebaseapp.com',
+  projectId: 'semanajsexpert4',
+  storageBucket: 'semanajsexpert4.appspot.com',
+  messagingSenderId: '303237998748',
+  appId: '1:303237998748:web:dd1ea366df04d1624ee559',
+  measurementId: 'G-NNP80P2PLJ',
+};
--- app/pages/login/src/app.js
+++ app/pages/login/src/app.js
@@ -1,39 +1,28 @@
+import { FIREBASE_CONFIG } from '../../_shared/constants.js';
+
 const btnLogin = document.getElementById('btnLogin');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD0vyC-dOjj-F-ewtvKdqkoS7jCpMwnWwM',
-  authDomain: 'semanajsexpert4.firebaseapp.com',
-  projectId: 'semanajsexpert4',
-  storageBucket: 'semanajsexpert4.appspot.com',
-  messagingSenderId: '303237998748',
-  appId: '1:303237998748:web:dd1ea366df04d1624ee559',
-  measurementId: 'G-NNP80P2PLJ',
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(FIREBASE_CONFIG);
 firebase.analytics();
 
-const provider = new firebase.auth.GithubAuthProvider();
+async function onLogin() {
+  try {
+    const provider = new firebase.auth.GithubAuthProvider();
 
-provider.addScope('read:user');
+    provider.addScope('read:user');
 
-function _login() {
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      const credential = result.credential;
-      const token = credential.accessToken;
+    const { user } = await firebase.auth().signInWithPopup(provider);
 
-      const user = result.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = error.credential;
-    });
+    const userData = {
+      img: user.photoURL,
+      username: user.displayName,
+    };
+
+    window.location.href = '/pages/lobby';
+  } catch (err) {
+    alert(err.message);
+    console.error(err);
+  }
 }
 
-btnLogin.addEventListener('click', _login);
+btnLogin.addEventListener('click', onLogin);
